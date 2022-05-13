15a007bac0e3beffe8d9b9fa90d398b406cd29b5
bokonist <jayrm1995@gmail.com>
2022-05-14 01:10:13 +0530

    added high level component structure

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,20 @@
 import '../styles/App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+import SideBar from './SideBar';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  return <div className="App">hello</div>;
+  const { selectedTab } = useState('Dashboard');
+  return (
+    <div className="App">
+      <NavBar />
+      <SideBar />
+      <Content selectedTab={selectedTab} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function SideBar() {
+  return (
+    <div>SideBar</div>
+  );
+}
+export default SideBar;
