ff40e692ebc3bd33d431333381cd1728cc91323d
bokonist <jayrm1995@gmail.com>
2022-05-14 04:13:54 +0530

    barebones navbar + styles

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import navbarLogo from '../assets/navbarLogo.png';
+import notificationBell from '../assets/notificationBell.png';
+import '../styles/NavBar.css';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className="navbar-container">
+      <img src={navbarLogo} alt="CORKOWL logo" className="navbar-logo" />
+      <input type="text" placeholder="Search wines, customers here..." />
+      <img src={notificationBell} alt="notifications" className="navbar-notification" />
+      <span>Jay Pillai</span>
+      <img src={notificationBell} alt="profile" className="navbar-profile" />
+    </div>
   );
 }
 
