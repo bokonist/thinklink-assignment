e04864dbafb00915ac25a1d0daa1cdc9840c5979
bokonist <jayrm1995@gmail.com>
2022-05-15 02:01:02 +0530

    chart header+ styling done

--- src/Components/ChartView.jsx
+++ src/Components/ChartView.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import refreshIcon from '../assets/refresh.svg';
 import '../styles/ChartView.css';
+import chartRise from '../assets/chartRise.svg';
+import chartRiseWithAxes from '../assets/chartRiseWithAxes.svg';
 
 function ChartView() {
   return (
@@ -21,32 +23,51 @@ function ChartView() {
         <form>
           <label htmlFor="filter-range" className="filter-container">
             <p className="filter-title">Filter by</p>
+            <p className="filter-divider">|</p>
             <select
               name="filter by date range"
               id="filter-range"
               className="filter-range-select"
             >
-              <option value="week">this week</option>
-              <option value="month">this month</option>
-              <option value="year">this year</option>
+              <option value="week">This week</option>
+              <option value="month">This month</option>
+              <option value="year">This year</option>
             </select>
           </label>
         </form>
       </div>
       <div className="chart-view-body-container">
         <div className="graph-container">
-          <div className="graph-info-container">graph info</div>
+          <div className="graph-info-container">
+            <div className="graph-numbers-container">
+              <p className="graph-profit">$74,729.00</p>
+              <div className="graph-insight-container">
+                <img
+                  src={chartRise}
+                  alt="graph insight icon"
+                  className="graph-insight-icon"
+                />
+                <p className="graph-insight">+21% from last week</p>
+              </div>
+            </div>
+            <div className="graph-general-insight-container">
+              <img
+                src={chartRiseWithAxes}
+                alt=""
+                className="graph-general-insight-icon"
+              />
+              <p className="graph-general-insight">
+                Highest revenue since 2 weeks ago
+              </p>
+            </div>
+          </div>
           <div className="graph-container">graph</div>
         </div>
         <div className="general-info-container">
           <h1 className="info-title">Total Profit</h1>
           <p className="net-info-container">$12,545.00</p>
           <div className="insight-container">
-            <img
-              src={refreshIcon}
-              alt="insights icon"
-              className="insight-icon"
-            />
+            <img src={chartRise} alt="insights icon" className="insight-icon" />
             <p className="insight-info">+23% from last week</p>
           </div>
           <h1 className="sales-info-title">Total Products Sold</h1>
