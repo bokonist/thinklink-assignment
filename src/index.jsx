8c7df27a19c78d297496671c87500405c111c4ad
bokonist <jayrm1995@gmail.com>
2022-05-14 05:31:11 +0530

    added css reset

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import './styles/reset.css';
 import App from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
