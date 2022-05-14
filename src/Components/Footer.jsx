f749cda9c5a719e60ebee141f6765acca20e9077
bokonist <jayrm1995@gmail.com>
2022-05-15 01:59:55 +0530

    footer+styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <h1 className="footer-title">CorkOwl</h1>
+      <p className="footer-subtitle">© 2020 CorkOwl, All Rights Reserved.</p>
+    </div>
   );
 }
 function shouldNotRerender() {
